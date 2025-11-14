bbe93d6cc4debf5e5ab7de03084559ca6f510599
aditya-waghmare1 <your-email@example.com>
2025-11-14 09:40:09 +0530

    Second commit

---
+++ Backend/routes/user.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const pool = require('../utils/db')
+
+const router = express.Router()
+
+
+router.get("/",(req,res)=>{
+    res.send("hello")
+})
+
+router.post('/login', (req, res) => {
+    const { email, password } = req.body
+    const sql = `SELECT * FROM users WHERE email = ?`
+    pool.query(sql, [email],  (error, data) => {
+        if (data != '') {
+            
+            res.send({"status":"ok","data":data})
+
+        }
+            
+            else
+            {
+                res.send("Invalid")
+            }
+                
+        }
+    
+            
+    )
+})
+
+router.post('/register', async (req, res) => {
+    const { fname, lname, email, mobile_no, dob,password} = req.body
+    const sql = `INSERT INTO users(fname, lname, email, mobile_no, dob,password) VALUES(?,?,?,?,?,?)`
+    try {
+       
+        pool.query(sql, [fname, lname, email, mobile_no, dob,password], (error, data) => {
+            res.send("Creted sucesfully")
+        })
+    }
+    catch (error) {
+        res.send(error)
+    }
+
+
+})
+
+
+
+
+
+
+
+module.exports = router
